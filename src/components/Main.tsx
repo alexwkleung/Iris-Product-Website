65ccfdbf0e149df121db5f7428c67ebb5cd28ce7
Alex Leung <alexleung25@outlook.com>
2023-07-09 23:14:01 -0700

    Added download installer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import '../assets/Main.css' 
 import img1 from '../assets/img/img1.png?inline?url';
 import img2 from '../assets/img/img2.png?inline?url';
+import irisDmg from '../assets/installers/iris-0.2.0-dev-4.1.dmg?inline?url'
 
 export function Main(): JSX.Element {
     const downloadVersion: string = "v0.2.0-dev-4.1 (macOS only)";
@@ -8,14 +9,19 @@ export function Main(): JSX.Element {
     return (
         <>
             <div className="box first">
-                <div className="heading-main"><div className="iris-text-heading-main">Iris</div>is a comfortable Markdown note-taking app</div>
+                <div className="heading-main">
+                    <div className="iris-text-heading-main">Iris</div>
+                    A comfortable Markdown note-taking app
+                </div>
                 <div className="box-first-img-container">
                     <img className="img1" src={img1}></img>
                     <img className="img1" src={img2}></img>
                 </div>
                 <div className="download-app">
-                    <div className="download-app-text-1">Download</div>
-                    <div className="download-version-text">{downloadVersion}</div>
+                    <a className="installer-link" href={irisDmg}>
+                        <div className="download-app-text-1">Download</div>
+                        <div className="download-version-text">{downloadVersion}</div>
+                    </a>
                 </div>
             </div>
             <div className="box second"></div>
